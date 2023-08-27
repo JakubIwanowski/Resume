d681b40e10ca188835bbc1699bd71cab03387584
Jakub <jakub@localhost.localdomain>
2023-08-28 00:22:44 +0200

    first one

---
+++ .eleventy.js
@@ -0,0 +1,39 @@
+const sass = require("sass");
+const path = require("node:path");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("img");
+  // Recognize Sass as a "template languages"
+  eleventyConfig.addTemplateFormats("scss");
+
+  // Compile Sass
+  eleventyConfig.addExtension("scss", {
+    outputFileExtension: "css",
+    compile: async function (inputContent, inputPath) {
+      // Skip files like _fileName.scss
+      let parsed = path.parse(inputPath);
+      if (parsed.name.startsWith("_")) {
+        return;
+      }
+
+      // Run file content through Sass
+      let result = sass.compileString(inputContent, {
+        loadPaths: [parsed.dir || "."],
+        sourceMap: false, // or true, your choice!
+      });
+
+      // Allow included files from @use or @import to
+      // trigger rebuilds when using --incremental
+      this.addDependencies(inputPath, result.loadedUrls);
+
+      return async () => {
+        return result.css;
+      };
+    },
+  });
+  // Creates the extension for use
+  return {
+    dataTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+  };
+};
